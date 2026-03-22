573a6a293664a0cf644e5b850dbdc314e4b0cc9a
SamuelSotomayor1 <andressotomayor477@gmail.com>
2026-03-22 05:22:13 -0300

    feat: the users controller are updated

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Role } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') //Ruta base
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll(@Query('role') role?: Role) {
+    return this.usersService.findAll(role);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDTO: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDTO);
+  }
+
+  @Delete('id')
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.remove(id);
+  }
+}
